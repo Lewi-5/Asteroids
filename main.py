378e64862a6ab972b48a656a94c90fc9ed16a181
Lewi-5 <Lewi-5@github.com>
2024-09-02 21:50:54 -0400

    turning with a and d

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2) ) 
+    
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -11,12 +18,18 @@ def main():
 
     game_on = True
     while game_on:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((000,000, 000))
-        pygame.display.flip
+        
+        player.update(dt)
+        player.draw(screen)        
+        pygame.display.flip()
+
+        dt = (game_clock.tick(60) / 1000)
 
 
 if __name__ == "__main__":
